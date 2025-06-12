fb8b50148f556702e377b9bc0c68f337414df710
MilicaRadojevic <milicaradojevic64@gmail.com>
2025-06-12 14:03:27 +0200

    feat: dinamically loading data from json file

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,3 +1,32 @@
+@php
+  $days = [
+      'Ponedeljak' => '08:00 - 16:00',
+      'Utorak' => '08:00 - 16:00',
+      'Sreda' => '08:00 - 16:00',
+      'Četvrtak' => '08:00 - 16:00',
+      'Petak' => '08:00 - 16:00',
+      'Subota' => '08:00 - 14:00',
+      'Nedelja' => 'neradna',
+  ];
+
+  $grouped = [];
+  foreach ($days as $day => $hours) {
+      $grouped[$hours][] = $day;
+  }
+
+  $formattedSchedule = [];
+  foreach ($grouped as $hours => $daysGroup) {
+      $dayLabels = array_values($daysGroup);
+      $dayCount = count($dayLabels);
+
+      if ($dayCount > 1) {
+          $formattedSchedule[] = $dayLabels[0] . ' - ' . $dayLabels[$dayCount - 1] . ': ' . $hours;
+      } else {
+          $formattedSchedule[] = $dayLabels[0] . ': ' . $hours;
+      }
+  }
+@endphp
+
 <footer class="bg-white dark:bg-gray-900">
   <div class="mx-auto w-full max-w-screen-xl p-4 py-6 lg:py-8">
     <div class="flex flex-col md:flex-row md:justify-between md:flex-wrap gap-6">
@@ -6,11 +35,11 @@
         <a class="flex flex-col items-start">
           <img class="h-8 mb-2" alt="LOGO" />
           <span class="text-2xl font-semibold whitespace-normal max-w-xs break-words dark:text-white">
-            Narodna biblioteka "Dositej Obradović" Novi Pazar
+            {{ $libraryData['name'] ?? '' }}
           </span>
           <div class="mt-3 text-sm text-gray-600 dark:text-gray-400 leading-relaxed">
-            <p>Adresa: Stevana Nemanje 2, 36300 Novi Pazar</p>
-            <p>PIB: 101234567</p>
+            <p> Adresa: {{ $libraryData['address'] ?? '' }}</p>
+            <p>PIB: {{ $libraryData['pib'] ?? '' }}</p>
           </div>
         </a>
       </div>
@@ -18,26 +47,28 @@
       <div class="grid grid-cols-2 gap-8 sm:gap-6 sm:grid-cols-2 md:basis-1/3 flex-grow">
         <div>
           <h2 class="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">Radno vreme</h2>
-          <ul class="text-gray-500 dark:text-gray-400 font-medium">
-            <li>PON - SUB : 8:00 - 16:00</li>
-            <li>NED : neradna</li>
-          </ul>
+            <ul class="text-gray-500 dark:text-gray-400 font-medium">
+              @foreach ($formattedSchedule as $line)
+                <li>{{ $line }}</li>
+              @endforeach
+            </ul>
+
         </div>
 
         <div>
           <h2 class="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">Kontakt</h2>
           <ul class="text-gray-500 dark:text-gray-400 font-medium space-y-2 mb-4">
-            <li><i class="fas fa-phone me-2"></i> +381 (0)20 331 010</li>
-            <li><i class="fas fa-envelope me-2"></i> dositejbib@gmail.com</li>
+            <li><i class="fas fa-phone me-2"></i> {{ $libraryData['phone'] ?? '' }} </li>
+            <li><i class="fas fa-envelope me-2"></i> {{ $libraryData['email'] ?? '' }}</li>
           </ul>
 
           <div class="flex space-x-4 mt-2">
-            <a href="#" class="text-gray-500 hover:text-gray-900 dark:hover:text-white" aria-label="Facebook page">
+            <a href="{{ $libraryData['facebook'] }}</" class="text-gray-500 hover:text-gray-900 dark:hover:text-white" aria-label="Facebook page">
               <svg class="w-6 h-6" fill="currentColor" viewBox="0 0 8 19" xmlns="http://www.w3.org/2000/svg">
                 <path fill-rule="evenodd" d="M6.135 3H8V0H6.135a4.147 4.147 0 0 0-4.142 4.142V6H0v3h2v9.938h3V9h2.021l.592-3H5V3.591A.6.6 0 0 1 5.592 3h.543Z" clip-rule="evenodd"/>
               </svg>
             </a>
-            <a href="#" class="text-gray-500 hover:text-gray-900 dark:hover:text-white" aria-label="Twitter page">
+            <a href="{{ $libraryData['twitter'] }}</" class="text-gray-500 hover:text-gray-900 dark:hover:text-white" aria-label="Twitter page">
               <svg class="w-6 h-6" fill="currentColor" viewBox="0 0 20 17" xmlns="http://www.w3.org/2000/svg">
                 <path fill-rule="evenodd" d="M20 1.892a8.178 8.178 0 0 1-2.355.635 4.074 4.074 0 0 0 1.8-2.235 8.344 8.344 0 0 1-2.605.98A4.13 4.13 0 0 0 13.85 0a4.068 4.068 0 0 0-4.1 4.038 4 4 0 0 0 .105.919A11.705 11.705 0 0 1 1.4.734a4.006 4.006 0 0 0 1.268 5.392 4.165 4.165 0 0 1-1.859-.5v.05A4.057 4.057 0 0 0 4.1 9.635a4.19 4.19 0 0 1-1.856.07 4.108 4.108 0 0 0 3.831 2.807A8.36 8.36 0 0 1 0 14.184 11.732 11.732 0 0 0 6.291 16 11.502 11.502 0 0 0 17.964 4.5c0-.177 0-.35-.012-.523A8.143 8.143 0 0 0 20 1.892Z" clip-rule="evenodd"/>
               </svg>
@@ -53,7 +84,7 @@
 
       <div class="mt-6 md:mt-0 md:basis-1/4 w-full md:w-auto">
         <iframe
-          src="https://www.google.com/maps?q=Stevana+Nemanje+2,+Novi+Pazar&output=embed"
+          src="{{ $libraryData['map_embed'] }}"
           width="100%"
           height="200"
           style="border:0;"
@@ -69,7 +100,7 @@
     <hr class="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
 
     <div class="text-center mt-4 text-sm text-gray-500 dark:text-gray-400">
-      © 2023 <a href="https://flowbite.com/" class="hover:underline">Narodna biblioteka "Dositej Obradović"</a>.
+      {{ $libraryData['copyrights'] }} <a href="#" class="hover:underline">{{ $libraryData['name'] }} </a>.
     </div>
   </div>
 </footer>
