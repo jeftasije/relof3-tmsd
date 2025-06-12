e7f406f4339591cca77220610fde6093ba11262d
Tidzi11 <128375609+Tidzi11@users.noreply.github.com>
2025-06-12 15:51:47 +0200

    Update guest.blade.php

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -24,10 +24,10 @@
 
             <!--<div class="w-full sm:max-w-md mt-6 px-6 py-4 bg-white dark:bg-gray-800 shadow-md overflow-hidden sm:rounded-lg">
                 {{ $slot }}
-            </div>--> <!-- ovo je za fon-->
+            </div>--> 
             <div class="w-full max-w-4xl mt-6 px-8 py-6 bg-white dark:bg-gray-800 shadow-md overflow-hidden rounded-lg">
                 {{ $slot }}
-            </div> <!-- ovo je za komp-->
+            </div> 
 
         </div>
     </body>
