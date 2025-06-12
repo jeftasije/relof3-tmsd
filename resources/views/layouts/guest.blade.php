03c6515afd478b6ba12a3324d47c3b82731d77c4
Tijana Lazic <tijanaticalazic02@gmail.com>
2025-06-12 15:17:01 +0200

    feat: services page

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -22,9 +22,13 @@
                 </a>
             </div>
 
-            <div class="w-full sm:max-w-md mt-6 px-6 py-4 bg-white dark:bg-gray-800 shadow-md overflow-hidden sm:rounded-lg">
+            <!--<div class="w-full sm:max-w-md mt-6 px-6 py-4 bg-white dark:bg-gray-800 shadow-md overflow-hidden sm:rounded-lg">
                 {{ $slot }}
-            </div>
+            </div>--> <!-- ovo je za fon-->
+            <div class="w-full max-w-4xl mt-6 px-8 py-6 bg-white dark:bg-gray-800 shadow-md overflow-hidden rounded-lg">
+                {{ $slot }}
+            </div> <!-- ovo je za komp-->
+
         </div>
     </body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -17,4 +17,8 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
+Route::get('/usluge', function () {
+    return view('user.services');
+});
+
 require __DIR__.'/auth.php';
