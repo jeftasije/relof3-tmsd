78784904df3b501d1d640d916fe0d218b25e1ff4
Tijana Lazic <tijanaticalazic02@gmail.com>
2025-06-18 22:16:31 +0200

    bug fix: fixing migrations

--- database/migrations/2025_06_13_111841_create_comments_table.php
+++ database/migrations/2025_06_13_111841_create_comments_table.php
@@ -6,17 +6,23 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
+    /**
+     * Run the migrations.
+     */
     public function up(): void
     {
         Schema::create('comments', function (Blueprint $table) {
             $table->id();
-            $table->string('name');    
-            $table->text('content');     
-            $table->timestamps();        
+            $table->string('author');
+            $table->text('content');
+            $table->timestamps();
         });
 
     }
 
+    /**
+     * Reverse the migrations.
+     */
     public function down(): void
     {
         Schema::dropIfExists('comments');
