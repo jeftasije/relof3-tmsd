900bbd3de5af9580946ec4a11d29f6c9c470922f
Stefan Nikolovski <nikolovski.stefan14@gmail.com>
2025-06-19 15:03:46 +0200

    bug fix: fixed theme bug

--- resources/js/app.js
+++ resources/js/app.js
@@ -9,3 +9,25 @@ import Alpine from 'alpinejs';
 
 window.Alpine = Alpine;
 Alpine.start();
+
+document.addEventListener('DOMContentLoaded', () => {
+    const darkIcons = document.querySelectorAll('#theme-toggle-dark-icon, #theme-toggle-dark-icon-mobile');
+    const lightIcons = document.querySelectorAll('#theme-toggle-light-icon, #theme-toggle-light-icon-mobile');
+    const toggleButtons = document.querySelectorAll('#theme-toggle, #theme-toggle-mobile');
+
+    const isDark = localStorage.getItem('color-theme') === 'dark' ||
+        (!localStorage.getItem('color-theme') && window.matchMedia('(prefers-color-scheme: dark)').matches);
+
+    document.documentElement.classList.toggle('dark', isDark);
+    darkIcons.forEach(icon => icon.classList.toggle('hidden', isDark));
+    lightIcons.forEach(icon => icon.classList.toggle('hidden', !isDark));
+
+    const toggleTheme = () => {
+        const isDark = document.documentElement.classList.toggle('dark');
+        localStorage.setItem('color-theme', isDark ? 'dark' : 'light');
+        darkIcons.forEach(icon => icon.classList.toggle('hidden', isDark));
+        lightIcons.forEach(icon => icon.classList.toggle('hidden', !isDark));
+    };
+
+    toggleButtons.forEach(button => button.addEventListener('click', toggleTheme));
+});
--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -145,31 +145,6 @@
 
 <script>
   document.addEventListener('DOMContentLoaded', () => {
-    const darkIcons = document.querySelectorAll('#theme-toggle-dark-icon, #theme-toggle-dark-icon-mobile');
-    const lightIcons = document.querySelectorAll('#theme-toggle-light-icon, #theme-toggle-light-icon-mobile');
-    const toggleButtons = document.querySelectorAll('#theme-toggle, #theme-toggle-mobile');
-
-    const isDark = localStorage.getItem('color-theme') === 'dark' ||
-      (!localStorage.getItem('color-theme') && window.matchMedia('(prefers-color-scheme: dark)').matches);
-
-    document.documentElement.classList.toggle('dark', isDark);
-    darkIcons.forEach(icon => icon.classList.toggle('hidden', isDark));
-    lightIcons.forEach(icon => icon.classList.toggle('hidden', !isDark));
-
-    const toggleTheme = () => {
-      const isDark = document.documentElement.classList.toggle('dark');
-      localStorage.setItem('color-theme', isDark ? 'dark' : 'light');
-      darkIcons.forEach(icon => icon.classList.toggle('hidden', isDark));
-      lightIcons.forEach(icon => icon.classList.toggle('hidden', !isDark));
-    };
-
-    toggleButtons.forEach(button => button.addEventListener('click', toggleTheme));
-  });
-
-
-
-
-  document.addEventListener('DOMContentLoaded', () => {
     const buttons = document.querySelectorAll('#mega-menu-full [data-collapse-toggle]');
     let activeCollapse = null;
 
